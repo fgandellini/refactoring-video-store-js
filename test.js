6d74823888f89655401991c6a3de30ef81358a6a
Federico Gandellini <fgandellini@gmail.com>
2018-01-25 20:40:36 +0100

    add some more tests to have a better coverage of edge cases

--- test.js
+++ test.js
@@ -4,9 +4,11 @@ const statement = require('./index')
 const MOVIES = {
   F001: { title: 'Ran', code: 'regular'},
   F002: { title: 'Trois Couleurs: Bleu', code: 'regular'},
+  F003: { title: 'Logan', code: 'new' },
+  F004: { title: 'Robin Hood', code: 'childrens' },
 }
 
-const customer = {
+const customer1 = {
   name: 'martin',
   rentals: [
     { movieID: 'F001', days: 3 },
@@ -14,7 +16,7 @@ const customer = {
   ]
 }
 
-const output =
+const output1 =
 'Rental Record for martin' +
 '\n\tRan\t3.5' +
 '\n\tTrois Couleurs: Bleu\t2' +
@@ -22,8 +24,50 @@ const output =
 '\nYou earned 2 frequent renter points' +
 '\n'
 
+const customer2 = {
+  name: 'fede',
+  rentals: [
+    { movieID: 'F003', days: 4 },
+    { movieID: 'F004', days: 5 },
+  ]
+}
+
+const output2 =
+'Rental Record for fede' +
+'\n\tLogan\t12' +
+'\n\tRobin Hood\t4.5' +
+'\nAmount owed is 16.5' +
+'\nYou earned 3 frequent renter points' +
+'\n'
+
+const customer3 = {
+  name: 'rob',
+  rentals: [
+    { movieID: 'F001', days: 1 },
+    { movieID: 'F004', days: 2 },
+  ]
+}
+
+const output3 =
+'Rental Record for rob' +
+'\n\tRan\t2' +
+'\n\tRobin Hood\t1.5' +
+'\nAmount owed is 3.5' +
+'\nYou earned 2 frequent renter points' +
+'\n'
+
 describe('statement', () => {
-  it('should work', () => {
-    assert.equal(output, statement(customer, MOVIES))
+
+  it('should print statement for customer 1', () => {
+    assert.equal(output1, statement(customer1, MOVIES))
   })
+
+  it('should print statement for customer 2', () => {
+    assert.equal(output2, statement(customer2, MOVIES))
+  })
+
+  it('should print statement for customer 3', () => {
+    assert.equal(output3, statement(customer3, MOVIES))
+  })
+
 })
